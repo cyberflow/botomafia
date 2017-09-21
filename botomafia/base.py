52018727a3de181436dec4ee60cfbbb68c00b506
Dmitry <dmitry.r@cyberflow.net>
2017-09-21 12:18:31 +0300

    mafia voting togrther

--- botomafia/base.py
+++ botomafia/base.py
@@ -107,7 +107,7 @@ class Mafia(Role):
     role = "Mafia"
 
     def day_vote(self):
-        return random.choice(self.game.list_players(skip=self.name))
+        return self.game.list_players(skip=self.mafia)[0]
 
     def mafia_night_meet(self, mafia):
         self.mafia = [m.name for m in mafia]
